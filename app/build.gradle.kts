2c1e7f60ac92f8de9cd1dedc38a136287cc65c3e
Abshiro Elmi <absaato@gmail.com>
2026-02-15 14:56:31 +0300

    12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.autov.sms"
         minSdk = 24
         targetSdk = 34
-        versionCode = 11
+        versionCode = 12
         versionName = "1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
